747152f5b076c964f74c8a3cdbac0eed674898d2
Nguyen-Hoang-Ky-Anh <23130010@st.hcmuaf.edu.vn>
2025-11-22 08:31:12 +0700

    update: Thêm nút trở về đầu trang và cập nhật hình ảnh

--- js/product-card.js
+++ js/product-card.js
@@ -1,7 +1,14 @@
 document.querySelectorAll('.add-to-cart-btn').forEach(btn => {
   btn.addEventListener('click', (event) => {
     event.stopPropagation();
-    window.location.href = 'cart.html';
+
+    // Thêm class animation
+    btn.classList.add('adding');
+
+    // Sau 700ms mới chuyển trang để nhìn thấy hiệu ứng
+    setTimeout(() => {
+      window.location.href = 'cart.html';
+    }, 700);
   });
 });
 
@@ -11,5 +18,4 @@ productCards.forEach(card => {
     window.location.href = "../pages/bookdetail.html"
   });
 });
-
 
