e7703201077f46c99562dbdcd7e012f1bf59c5ec
Nguyen-Hoang-Ky-Anh <23130010@st.hcmuaf.edu.vn>
2025-12-08 19:42:54 +0700

    UPDATE: Hoàn thiện tính năng login / signup + mã hóa mật khẩu của user khi họ đăng ký thành công

--- src/main/java/DAO/UserDAO.java
+++ src/main/java/DAO/UserDAO.java
@@ -1,24 +1,32 @@
 package DAO;
 
+import at.favre.lib.crypto.bcrypt.BCrypt;
+import models.User;
 import utils.DBConnection;
 
-import java.sql.Connection;
-import java.sql.PreparedStatement;
-import java.sql.ResultSet;
-import java.sql.SQLException;
+import java.sql.*;
+import java.util.ArrayList;
+import java.util.List;
 
 public class UserDAO {
     public boolean checkLogin(String usernameOrEmail, String password){
-        String sql = "select * from user where (username = ? or email = ?) and password = ?";
+        String sql = "SELECT password FROM user WHERE username = ? OR email = ? LIMIT 1";
 
-        try(Connection connection = DBConnection.getConnection();
-        PreparedStatement stm = connection.prepareStatement(sql)){
+        try (Connection connection = DBConnection.getConnection();
+             PreparedStatement stm = connection.prepareStatement(sql)) {
             stm.setString(1, usernameOrEmail);
             stm.setString(2, usernameOrEmail);
-            stm.setString(3, password);
 
             ResultSet rs = stm.executeQuery();
-            return rs.next();
+
+            if (rs.next()) {
+                String hashedPassword = rs.getString("password");
+
+                BCrypt.Result result = BCrypt.verifyer().verify(password.toCharArray(), hashedPassword);
+
+                return result.verified;
+            }
+
         } catch (Exception e) {
             e.printStackTrace();
         }
@@ -37,6 +45,85 @@ public class UserDAO {
             int rows = stm.executeUpdate();
             return rows > 0;
 
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+        return false;
+    }
+
+    public boolean checkAvailableUserNameOrEmail(String userNameOrEmail){
+        String sql = "select userName, email from user where userName = ? or email = ?";
+        try(Connection connection = DBConnection.getConnection();
+            PreparedStatement stm = connection.prepareStatement(sql)) {
+            stm.setString(1, userNameOrEmail);
+            stm.setString(2, userNameOrEmail);
+            ResultSet rs = stm.executeQuery();
+            return rs.next();
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+        return false;
+    }
+
+    public User getUserByID(int id){
+        String sql = "select * from user where id = ?";
+        try (Connection connection = DBConnection.getConnection();
+        PreparedStatement stm = connection.prepareStatement(sql)) {
+            stm.setInt(1, id);
+            ResultSet rs = stm.executeQuery();
+
+            if(rs.next()){
+                return new User(rs.getInt("id"),
+                                rs.getString("userName"),
+                                rs.getString("email"),
+                                rs.getString("phoneNum"),
+                                rs.getString("password"));
+            }
+        } catch (SQLException e){
+            e.printStackTrace();
+        }
+        return null;
+    }
+
+    public List<User> getAllUsers(){
+        String sql = "select * from user";
+        try(Connection connection = DBConnection.getConnection()) {
+            ResultSet rs = connection.prepareStatement(sql).executeQuery();
+            List<User> list = new ArrayList<>();
+            if(rs.next()){
+                list.add(new User(rs.getInt("id"),
+                        rs.getString("userName"),
+                        rs.getString("email"),
+                        rs.getString("phoneNum"),
+                        rs.getString("password")));
+            }
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+        return null;
+    }
+
+    public boolean updateUser(User user){
+        String sql = "UPDATE user SET userName=?, email=?, phoneNum=? WHERE id=?";
+        try (Connection connection = DBConnection.getConnection();
+        PreparedStatement stm = connection.prepareStatement(sql)){
+            stm.setString(1, user.getUsername());
+            stm.setString(2, user.getEmail());
+            stm.setString(3, user.getPhoneNum());
+            stm.setInt(4, user.getId());
+            return stm.executeUpdate() > 0;
+        } catch (SQLException e){
+            e.printStackTrace();
+        }
+        return false;
+    }
+
+    public boolean deleteUser(int id){
+        String sql = "DELETE from user where id=?";
+        try(Connection connection = DBConnection.getConnection();
+        PreparedStatement stm = connection.prepareStatement(sql)){
+            stm.setInt(1, id);
+            return stm.executeUpdate() > 0;
         } catch (SQLException e) {
             e.printStackTrace();
         }
