50c5744cac9d47ab8957ca9ef0aa3534d9af7663
Nguyen-Hoang-Ky-Anh <23130010@st.hcmuaf.edu.vn>
2026-01-17 10:08:30 +0700

    UPDATE: Cập nhật lại tính năng quên mật khẩu + fix bug của favorite-btn

--- src/main/webapp/assets/js/product-card.js
+++ src/main/webapp/assets/js/product-card.js
@@ -1,21 +1,23 @@
 document.querySelectorAll('.add-to-cart-btn').forEach(btn => {
-  btn.addEventListener('click', (event) => {
-    event.stopPropagation();
-
-    // Thêm class animation
-    btn.classList.add('adding');
-
-    // Sau 700ms mới chuyển trang để nhìn thấy hiệu ứng
-    setTimeout(() => {
-      window.location.href = 'cart.html';
-    }, 700);
-  });
+    btn.addEventListener('click', (event) => {
+        event.stopPropagation();
+        // Thêm class animation
+        btn.classList.add('adding');
+        // Sau 700ms mới chuyển trang để nhìn thấy hiệu ứng
+        setTimeout(() => {
+            window.location.href = ctx + '/cart';
+        }, 700);
+    });
 });
 
+// Xử lý click trên product-card
 const productCards = document.querySelectorAll('.product-card');
 productCards.forEach(card => {
-  card.addEventListener('click', () => {
-    window.location.href = ctx + "/bookdetail";
-  });
+    card.addEventListener('click', (e) => {
+        // Nếu click vào nút trái tim thì bỏ qua
+        if (e.target.classList.contains('favorite-btn') || e.target.closest('.favorite-btn')) {
+            return;
+        }
+        window.location.href = ctx + "/bookdetail";
+    });
 });
-
